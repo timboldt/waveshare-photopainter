e58e8f8cf91cd4632c65167052bff90d0c6969cc
Tim Boldt <tim.boldt@gmail.com>
2024-10-27 15:32:04 -0700

    Rough cut of display driver

--- async-fw-rs/src/main.rs
+++ async-fw-rs/src/main.rs
@@ -4,14 +4,42 @@
 use defmt::*;
 use embassy_executor::Spawner;
 use embassy_rp::gpio;
+use embassy_rp::spi::{Config, Spi};
 use embassy_time::{Duration, Timer};
-use gpio::{Level, Output};
+use gpio::{Input, Level, Output, Pull};
 use {defmt_rtt as _, panic_probe as _};
+
+mod epaper;
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     // Initialise Peripherals
     let p = embassy_rp::init(Default::default());
+
+    let miso = p.PIN_12;
+    let mosi = p.PIN_11;
+    let clk = p.PIN_10;
+
+    let mut spi = Spi::new(
+        p.SPI1,
+        clk,
+        mosi,
+        miso,
+        p.DMA_CH0,
+        p.DMA_CH1,
+        Config::default(),
+    );
+    let mut epaper = epaper::EPaper7In3F::new(
+        Output::new(p.PIN_13, Level::Low),
+        Output::new(p.PIN_14, Level::Low),
+        Output::new(p.PIN_15, Level::Low),
+        Input::new(p.PIN_16, Pull::Up), //XXXX
+        spi,
+    );
+
+    epaper.init().await.unwrap();
+    epaper.show_seven_color_blocks().await.unwrap();
+    epaper.deep_sleep().await.unwrap();
 
     // Create LED
     let mut led = Output::new(p.PIN_25, Level::Low);
